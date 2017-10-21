d0ccdeb4253d8e239812283792279575a8123713
scormrz <scormruiz@gmail.com>
2017-10-21 00:49:07 -0500

    Add files via upload

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,11 +1,164 @@
-// ConsoleApplication2.cpp: define el punto de entrada de la aplicación de consola.
-//
-
+// BomberManCpp.cpp: define el punto de entrada de la aplicación de consola.
 #include "stdafx.h"
+#include "iostream"
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
 
+
+#define ARRIBA 72
+#define ABAJO 80
+#define IZQUIERDA 75
+#define DERECHA 77
+
+using namespace std;
+
+
+int cc = 1, cf = 1;
+char tecla;
+/* Obtener caracter en posicion (x,y) */
+char get_char_at_xy(int x, int y) {
+	
+	
+
+	CHAR_INFO ci;
+	COORD xy = { 0, 0 };
+	SMALL_RECT rect = { x, y, x, y };
+	COORD coordBufSize = { 1,1 };
+	return ReadConsoleOutput(
+		GetStdHandle(STD_OUTPUT_HANDLE),
+		&ci,
+		coordBufSize,
+		xy,
+		&rect)
+		? ci.Char.AsciiChar
+		: '\0';
+}
+/* Posicionar cursor en posicion(x,y) */
+void gotoxy(int x, int y) {//Posicionar Cursor [#include<windows.h>]
+	HANDLE hcon;
+	hcon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hcon, dwPos);
+}
 
 int main()
 {
-    return 0;
-}
 
+	srand(time(NULL));
+	int num;
+
+	const int tam_x = 13;
+	const int tam_y = 16;
+
+
+
+	int mapa[tam_x][tam_y] =
+	{
+		{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 },
+		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,0,1,1,1,0,1,0,1,0,1,0,1,0,1 },
+		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,0,1,0,1,0,1,0,1,1,1,0,1,0,1 },
+		{ 1,0,1,0,0,0,0,0,0,0,0,1,0,0,1 },
+		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
+		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,1,1 },
+		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
+		{ 1,1,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,1,1,0,1,0,1,0,1,0,1,0,1,0,1 },
+		{ 1,0,0,0,1,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 }
+	};
+	
+
+	for (int i = 0; i<13; i++)
+	{
+		for (int j = 0; j<15; j++)
+		{
+			//Generamos un random para crear aleatoriamente bloques de premios (x), o fantasmas (f)
+			num = 1 + rand() % (101 - 1);
+
+			if (mapa[i][j] == 1)
+			{
+				printf("#");
+			}
+
+			if (mapa[i][j] == 0)
+			{
+				if(num>=0&&num<=90)
+					printf(" ");
+				if (num>90 && num<=95)
+					printf("x");
+				if (num > 95 && num < 100)
+					printf("f");
+					
+			}
+		}
+		printf("\n");
+	}
+
+	gotoxy(cc, cf);
+	cout << 'B';
+	while (tecla != 'x') {
+		tecla = _getch();//tecla = getch();
+		switch (tecla) {
+		case DERECHA:
+			if (get_char_at_xy(cc + 1, cf) != '#')
+			{
+				gotoxy(cc, cf);
+				cout << ' ';
+				cc++;
+				gotoxy(cc, cf);
+				cout << 'B';
+			}
+			else {
+				gotoxy(cc, cf);
+			}
+			break;
+		case IZQUIERDA:
+			if (get_char_at_xy(cc - 1, cf) != '#')
+			{
+				gotoxy(cc, cf);
+				cout << ' ';
+				cc--;
+				gotoxy(cc, cf);
+				cout << 'B';
+			}
+			else {
+				gotoxy(cc, cf);
+			}
+			break;
+		case ABAJO:
+			if (get_char_at_xy(cc, cf + 1) != '#')
+			{
+				gotoxy(cc, cf);
+				cout << ' ';
+				cf++;
+				gotoxy(cc, cf);
+				cout << 'B';
+			}
+			else {
+				gotoxy(cc, cf);
+			}
+			break;
+		case ARRIBA:
+			if (get_char_at_xy(cc, cf - 1) != '#')
+			{
+				gotoxy(cc, cf);
+				cout << ' ';
+				cf--;
+				gotoxy(cc, cf);
+				cout << 'B';
+			}
+			else {
+				gotoxy(cc, cf);
+			}
+			break;
+		}
+	}
+	return 0;
+}
